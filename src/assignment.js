c4efb1c3de5ba9f0ee21fde160d0f33ddf09a41c
Mololuwa Richard OLORUNSOLA <104454092+RICHARD-MOR@users.noreply.github.com>
2022-07-08 23:26:56 +0000

    challenge 1 challenge 2 challenge 3

--- src/assignment.js
+++ src/assignment.js
@@ -15,13 +15,34 @@ const assignment = {};
  */
 function sumOfNumbers(arrayOfNumbers) {
     let sum = 0;
-for(let i = 0; i<= arrayOfNumbers.length-1; i++){
+   for(let i = 0; i<= arrayOfNumbers.length-1; i++){
       sum += arrayOfNumbers[i];
    }
    return sum;
 }
-console.log(sumOfNumbers(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-assignment.sumOfNumbers=sumOfNumbers;
+ console.log(sumOfNumbers([1,2,3,4,5,6,7,8,9]));
+assignment.sumOfNumbers = sumOfNumbers;
+
+/**
+ * Challenge - 2
+ * 
+ * Count all even numbers within an array of numbers.
+ * If you did Challenge - 2, remove the comment in the line just after this function
+ * 
+ * @param {Array} arrayOfNumbers the array containing even or non-even numbers
+ * @returns number the count of even numbers
+ */
+ function countEvenNumbers(arrayOfNumbers) {
+  let numOfEvenNumbers = 0;
+  for (let numbers = 0;   numbers < arrayOfNumbers.length;  numbers++) {
+    if(arrayOfNumbers[numbers]%2==0) 
+      numOfEvenNumbers+=1;
+   }
+   let stmt = `the count of even numbers within the array is: ${numOfEvenNumbers} `
+   return stmt;
+ }
+//console.log(countEvenNumbers([1,2,3,4,5,6,7,8,9]));
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -37,21 +58,18 @@ assignment.sumOfNumbers=sumOfNumbers;
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
  */
-assignment.sumOfNumbers = sumOfNumbers;
-
 function celsiusToFahrenheit(arrayOfNumbers) {
-  let divisor = 9/5;
-   for(let i = 0; i < arrayOfNumbers.length; i++){
-     let multiplication = arrayOfNumbers[i] * divisor;
-     let fahrenheit = multiplication + 32;
-   let roundedValue = Math.round(fahrenheit);
-      arrayOfNumbers[i] = roundedValue;
-   }
-   return arrayOfNumbers;
+ let divisor = 9/5;
+  for(let i = 0; i < arrayOfNumbers.length; i++){
+    let multiplication = arrayOfNumbers[i] * divisor;
+    let fahrenheit = multiplication + 32;
+  let roundedValue = Math.round(fahrenheit);
+     arrayOfNumbers[i] = roundedValue;
   }
-assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+  return arrayOfNumbers;
+ }
   
- 
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
